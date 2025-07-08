bc2b3bec1deb26f4465dcadf6701dbf695799441
5ceansdeep <su5653@naver.com>
2025-07-08 16:07:48 +0900

    load / main 컴포넌트 나누기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Load from "./load"; // 파일 이름이 load.js니까 소문자 주의!
+import Load from "./load.js";
+import MainLogin from "./main.js";
 
 function App() {
   return (
     <div>
       <Load />
+      <MainLogin />
     </div>
   );
 }
